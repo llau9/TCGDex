bfb8aa279df3c741676ffc85ca6c0c1a39771440
rossg <rgonz552@fiu.edu>
2024-07-16 10:30:23 -0400

    Added .env file for API keys

--- my_app/lib/Frontend/main.dart
+++ my_app/lib/Frontend/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 import 'home_screen.dart';
 // Ensure SignInPage is correctly imported
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+  
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -36,6 +41,30 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const HomeScreen(), // Set HomeScreen as the default screen
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Access environment variables
+    String apiKey = dotenv.env['API_KEY'] ?? 'default_api_key';
+    String baseUrl = dotenv.env['BASE_URL'] ?? 'https://default.url.com';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter .env Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('API Key: $apiKey'),
+            Text('Base URL: $baseUrl'),
+          ],
+        ),
+      ),
     );
   }
 }
